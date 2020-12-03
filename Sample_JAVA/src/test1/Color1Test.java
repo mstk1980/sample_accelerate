19af3c36ef021de6e2fa59c1dc3c64c46e8081ff
mstk1980 <mstk1980@gmail.com>
2020-12-04 02:13:26 +0900

    renewal

---
+++ Sample_JAVA/src/test1/Color1Test.java
@@ -0,0 +1,27 @@
+package test1;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Color1Test {
+
+	@Test
+	public void testGetColor1() {
+		Color1 c1 = new Color1();
+		String t1 = c1.getColor(1);
+		assertThat(t1,is("赤"));
+	}
+	@Test
+	public void testGetColor2() {
+		Color1 c1 = new Color1();
+		String t1 = c1.getColor(2);
+		assertThat(t1,is("青"));
+	}
+	@Test
+	public void testGetColor3() {
+		Color1 c1 = new Color1();
+		String t1 = c1.getColor(3);
+		assertThat(t1,is("1or2を入力して下さい"));
+	}
+}
