aae890772daefad3883763b72211b6d546af3053
mstk1980 <mstk1980@gmail.com>
2020-12-03 14:11:20 +0900

    Add Run

---
+++ Sample_accelerate_java/src/test1/Color1Run.java
@@ -0,0 +1,14 @@
+package test1;
+
+public class Color1Run {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Color1 cl = new Color1();
+
+		//メソッドの呼出し
+		System.out.println(cl.getColor(1));
+
+	}
+
+}
